329593291daa21547887d1db6756aacf06fa2128
GIRISH MISHRA <girishmishra123456789@gmail.com>
2026-01-03 19:50:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (25.85%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,19 +1,34 @@
 class Solution {
-    public int solve(int row,int col,int m,int n,int[][] dp){
-        if(row>=m||col>=n) return 0;
-        if(row==m-1&&col==n-1) return 1;
-        if(dp[row][col]!=-1) return dp[row][col];
-        int down=solve(row+1,col,m,n,dp);
-        int right=solve(row,col+1,m,n,dp);
-        return dp[row][col]=down+right;
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+                if(i==0||j==0)dp[i][j]=1;
+                else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
             }
         }
-        return solve(0,0,m,n,dp);
+        return dp[m-1][n-1];
     }
 }
+
+// class Solution {
+//     public int solve(int row,int col,int m,int n,int[][] dp){
+//         if(row>=m||col>=n) return 0;
+//         if(row==m-1&&col==n-1) return 1;
+//         if(dp[row][col]!=-1) return dp[row][col];
+//         int down=solve(row+1,col,m,n,dp);
+//         int right=solve(row,col+1,m,n,dp);
+//         return dp[row][col]=down+right;
+//     }
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp=new int[m][n];
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 dp[i][j]=-1;
+//             }
+//         }
+//         return solve(0,0,m,n,dp);
+//     }
+// }
