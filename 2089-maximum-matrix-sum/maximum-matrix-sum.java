c0d781fa92b98a5091ace418b8144ba958d925ef
GIRISH MISHRA <girishmishra123456789@gmail.com>
2026-01-06 05:18:09 +0530

    Time: 7 ms (48.15%) | Memory: 68 MB (53.70%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        long sum=0;
+        int maxsum=Integer.MAX_VALUE;
+        int count_neg=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                sum=sum+Math.abs(matrix[i][j]);
+                if(matrix[i][j]<0){
+                    count_neg++;
+                }
+                maxsum=Math.min(maxsum,Math.abs(matrix[i][j]));
+            }
+        }
+        if(count_neg%2!=0){
+            return sum-2L*maxsum;
+        }
+        return sum;
+    }
+}
