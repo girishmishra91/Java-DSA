4896954f752fc1e1d66618f14e74b421b593ab4e
GIRISH MISHRA <girishmishra123456789@gmail.com>
2026-02-05 16:19:02 +0530

    Time: 7 ms (61.58%) | Memory: 47.4 MB (21.10%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canFinish(int n, int[][] pre) {
+        List<List<Integer>> adj=new ArrayList<>();
+        int[] indegree=new int[n];
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<pre.length;i++){
+            int a=pre[i][0],b=pre[i][1];
+            adj.get(b).add(a);
+            indegree[a]++;
+        }
+        //kahns algo
+        Queue<Integer> qu=new LinkedList<>();
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) qu.add(i);
+        }
+        while(qu.size()>0){
+            int front=qu.remove();
+            ans.add(front);
+            for(int ele:adj.get(front)){
+                indegree[ele]--;
+                if(indegree[ele]==0){
+                    qu.add(ele);
+                }
+            }
+        }
+        if(ans.size()==n) return true;
+        else return false;
+    }
+}
