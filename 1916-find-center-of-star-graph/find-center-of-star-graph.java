981852c1fe5aa60ed3dcc44f91f8b71861c95822
GIRISH MISHRA <girishmishra123456789@gmail.com>
2026-02-09 14:38:05 +0530

    Time: 0 ms (100.00%) | Memory: 111.1 MB (51.46%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,13 +1,10 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        int a= edges[0][0];
-        int b= edges[0][1];
-        int c= edges[1][0];
-        int d= edges[1][1];
-        if(a==c || a==d){
-            return a;
-
-        } 
-       return b ;
+        int a=edges[0][0];
+        int b=edges[0][1];
+        int c=edges[1][0];
+        int d=edges[1][1];
+        if(a==c||a==d) return a;
+        return b;
     }
 }
