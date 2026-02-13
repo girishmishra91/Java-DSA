dc5edc4a9b2670d509056c9ca760dc0b6879b70e
GIRISH MISHRA <girishmishra123456789@gmail.com>
2026-02-13 06:23:17 +0530

    Time: 10 ms (84.25%) | Memory: 49 MB (73.62%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -3,22 +3,22 @@ class Solution {
         int node;
         int time;
         Pair(int node,int time){
-            this.node=node;
             this.time=time;
+            this.node=node;
         }
         public int compareTo(Pair p){
-            if(this.time==p.time)return this.node-p.node;
+            if(this.time==p.time) return this.node-p.node;
             return this.time-p.time;
         }
-    } 
+    }
     public int networkDelayTime(int[][] times, int n, int src) {
         List<List<Pair>> adj=new ArrayList<>();
         for(int i=0;i<=n;i++){
             adj.add(new ArrayList<>());
         }
         for(int i=0;i<times.length;i++){
-            int u=times[i][0],v=times[i][1],time=times[i][2];
-            adj.get(u).add(new Pair(v,time));
+            int u=times[i][0],v=times[i][1],t=times[i][2];
+            adj.get(u).add(new Pair(v,t));
         }
         int[] ans=new int[n+1];
         Arrays.fill(ans,Integer.MAX_VALUE);
@@ -29,19 +29,20 @@ class Solution {
             Pair top=pq.remove();
             int node=top.node,time=top.time;
             if(top.time>ans[node]) continue;
-            for(Pair p:adj.get(node)){
-                int totalTime=top.time+p.time;
-                if(totalTime<ans[p.node]){
-                    ans[p.node]=totalTime;
-                    pq.add(new Pair(p.node,totalTime));
+            for(Pair p:adj.get(top.node)){
+                int totaltime=top.time+p.time;
+                if(totaltime<ans[p.node]){
+                    ans[p.node]=totaltime;
+                    pq.add(new Pair(p.node,totaltime));
                 }
             }
         }
-        int max=-1;
+        int maxi=-1;
         for(int i=1;i<=n;i++){
-            if(ans[i]==Integer.MAX_VALUE) return -1;
-            max=Math.max(max,ans[i]);
+            if(ans[i]==Integer.MAX_VALUE)return -1;
+            maxi=Math.max(maxi,ans[i]);
         }
-        return max;
+        return maxi;
+
     }
 }
