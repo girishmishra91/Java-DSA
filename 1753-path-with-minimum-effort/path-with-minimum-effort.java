4e0936d9d161a7aa5ad0fddf458457ad918a8fc2
GIRISH MISHRA <girishmishra123456789@gmail.com>
2026-02-16 11:40:10 +0530

    Time: 49 ms (41.42%) | Memory: 47.7 MB (91.82%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,6 +1,8 @@
 class Solution {
     public class Triplet implements Comparable<Triplet>{
-        int row, col, effort;
+        int row;
+        int col;
+        int effort;
         Triplet(int row,int col,int effort){
             this.row=row;
             this.col=col;
@@ -22,41 +24,23 @@ class Solution {
         ans[0][0]=0;
         PriorityQueue<Triplet> pq=new PriorityQueue<>();
         pq.add(new Triplet(0,0,0));
+        int[] r={-1,0,1,0};
+        int[] c={0,1,0,-1};
         while(pq.size()>0){
             Triplet top=pq.remove();
             int row=top.row;
             int col=top.col;
             int effort=top.effort;
-            if(row>0){//up
-                int e=Math.abs(heights[row][col]-heights[row-1][col]);
+            if(row==n-1&&col==m-1) break;
+            for(int i=0;i<4;i++){
+                int newrow=row+r[i];
+                int newcol=col+c[i];
+                if(newrow<0||newcol<0||newrow>=n||newcol>=m) continue;
+                int e=Math.abs(heights[row][col]-heights[newrow][newcol]);
                 e=Math.max(e,effort);
-                if(e<ans[row-1][col]){
-                    ans[row-1][col]=e;
-                    pq.add(new Triplet(row-1,col,e));
-                }
-            }
-            if(col>0){//left
-                int e=Math.abs(heights[row][col]-heights[row][col-1]);
-                e=Math.max(e,effort);
-                if(e<ans[row][col-1]){
-                    ans[row][col-1]=e;
-                    pq.add(new Triplet(row,col-1,e));
-                }
-            }
-            if(row<n-1){ // down
-                int e=Math.abs(heights[row][col]-heights[row+1][col]);
-                e=Math.max(e,effort);
-                if(e<ans[row+1][col]){
-                    ans[row+1][col]=e;
-                    pq.add(new Triplet(row+1,col,e));
-                }
-            }
-            if(col<m-1){  //right
-                int e=Math.abs(heights[row][col]-heights[row][col+1]);
-                e=Math.max(e,effort);
-                if(e<ans[row][col+1]){
-                    ans[row][col+1]=e;
-                    pq.add(new Triplet(row,col+1,e));
+                if(e<ans[newrow][newcol]){
+                    ans[newrow][newcol]=e;
+                    pq.add(new Triplet(newrow,newcol,e));
                 }
             }
         }
