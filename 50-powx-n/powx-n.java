73e55444c80dabf8f3c747b8e6ab002fda099339
GIRISH MISHRA <girishmishra123456789@gmail.com>
2026-02-21 11:55:23 +0530

    Time: 1 ms (9.74%) | Memory: 48.5 MB (10.69%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double power(double x,int n){
+        if(n==0) return 1.0;
+        double ans=power(x,n/2);
+        if(n%2==0)return ans*ans;
+        else return ans*ans*x;
+    }
+    public double myPow(double x, int n) {
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        return power(x,n);
+    }
+}
